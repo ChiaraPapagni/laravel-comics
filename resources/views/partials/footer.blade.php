2c6fec7ee1c7631809f9d32e7947dfae42e86774
Chiara <chiaraiors@gmail.com>
2021-12-17 17:18:27 +0100

    complete footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,82 @@
 <footer id="site_footer">
-    Footer
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <h3>DC Comics</h3>
+                    <ul>
+                        <li><a href="">Characters</a></li>
+                        <li><a href="">Comics</a></li>
+                        <li><a href="">Movies</a></li>
+                        <li><a href="">TV</a></li>
+                        <li><a href="">Games</a></li>
+                        <li><a href="">Video</a></li>
+                        <li><a href="">News</a></li>
+                    </ul>
+                    <h3>Shop</h3>
+                    <ul>
+                        <li><a href="">Shop DC</a></li>
+                        <li><a href="">Shop DC Collectibles</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>DC</h3>
+                    <ul>
+                        <li><a href="">Terms Of Use</a></li>
+                        <li><a href="">Privacy policy (New)</a></li>
+                        <li><a href="">Ad Choices</a></li>
+                        <li><a href="">Advertising</a></li>
+                        <li><a href="">Jobs</a></li>
+                        <li><a href="">Subscriptions</a></li>
+                        <li><a href="">Talent Workshops</a></li>
+                        <li><a href="">CPSC Certificates</a></li>
+                        <li><a href="">Rating</a></li>
+                        <li><a href="">Shop Help</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Sites</h3>
+                    <ul>
+                        <li><a href="">DC</a></li>
+                        <li><a href="">MAD Magazine</a></li>
+                        <li><a href="">DC Kids</a></li>
+                        <li><a href="">DC Universe</a></li>
+                        <li><a href="">DC Power Visa</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="credit">
+                <p>
+                    All Site Content TM and &copy; 2020 DC Entertainment, unless otherwise <a href="">noted here</a>.
+                    All right reserved.
+                </p>
+                <p>
+                    <a href="">Cookies Settings</a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <!-- /.footer_top -->
+
+    <div class="footer_bottom">
+        <div class="container">
+            <div class="sign_up">
+                <a href="">Sign-up now!</a>
+            </div>
+            <div class="social">
+                <h3>Follow us</h3>
+                <img src="{{asset('img/footer-facebook.png')}}" alt="facebook" />
+                <img src="{{asset('img/footer-twitter.png')}}" alt="twitter" />
+                <img src="{{asset('img/footer-youtube.png')}}" alt="youtube" />
+                <img src="{{asset('img/footer-pinterest.png')}}" alt="pinterest" />
+                <img src="{{asset('img/footer-periscope.png')}}" alt="periscope" />
+            </div>
+        </div>
+    </div>
+    <!-- /.footer_bottom -->
 </footer>
 <!-- /#site_footer -->
